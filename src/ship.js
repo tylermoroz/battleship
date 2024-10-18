b6fa2e79dd2974aabc228fd314473d397065adc9
tylermoroz <moroztyler@gmail.com>
2024-10-18 15:11:37 -0600

    Add isSunk method to calculate and change sunk property. Add test for isSunk and organize hit behavior tests.

--- src/ship.js
+++ src/ship.js
@@ -1,11 +1,18 @@
 export class Ship {
-  constructor() {
-    this.length = 0;
+  constructor(length) {
+    this.length = length;
     this.timesHit = 0;
     this.sunk = false;
   }
 
   hit() {
     this.timesHit++;
+    this.isSunk();
+  }
+
+  isSunk() {
+    if (this.timesHit === this.length) {
+      this.sunk = true;
+    }
   }
 }
--- tests/ship.test.js
+++ tests/ship.test.js
@@ -4,11 +4,30 @@ describe("Ship", () => {
   let ship;
 
   beforeEach(() => {
-    ship = new Ship();
+    ship = new Ship(3);
   });
 
-  test("increase the timesHit property by 1 for every successful call", () => {
-    ship.hit();
-    expect(ship.timesHit).toBe(1);
+  describe("Ship initialization", () => {
+    test("ship object properties on creation", () => {
+      expect(ship.length).toBe(3);
+      expect(ship.timesHit).toBe(0);
+      expect(ship.sunk).toBe(false);
+    });
+  });
+
+  describe("Hit behavior", () => {
+    test("increments timesHit for each successful hit", () => {
+      ship.hit();
+      ship.hit();
+      ship.hit();
+      expect(ship.timesHit).toBe(3);
+    });
+
+    test("marks ship as sunk when timesHit equals length", () => {
+      ship.hit();
+      ship.hit();
+      ship.hit();
+      expect(ship.sunk).toBe(true);
+    });
   });
 });
